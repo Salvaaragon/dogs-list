f308d950d2b2f481fe76e0e9b2aeb15d5736323b
Salva Aragón Reyes <salva.aragonreyes@gmail.com>
2021-03-17 20:37:17 +0100

    feat: configure router and add into App and Layout components

--- src/containers/Layout/Content/styles.ts
+++ src/containers/Layout/Content/styles.ts
@@ -3,4 +3,6 @@ import styled from 'styled-components';
 
 export const ContentContainer = styled(Container)`
   background-color: ${(props) => props.theme.palette.background.default};
+  padding-top: 5rem;
+  padding-bottom: 5rem;
 `;
