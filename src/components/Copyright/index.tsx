c02201f5463970e428fd0ca587f2488b825bf96a
Salva Aragón Reyes <salva.aragonreyes@gmail.com>
2021-03-17 20:32:14 +0100

    refactor: update copyright component for display flex text

--- src/components/Copyright/index.tsx
+++ src/components/Copyright/index.tsx
@@ -2,6 +2,7 @@ import CustomText from '@Components/CustomText';
 import { Box, Grid } from '@material-ui/core';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { CopyrightInfoGrid } from './styles';
 
 const Copyright: React.FC = () => {
   const { i18n } = useTranslation();
@@ -10,14 +11,24 @@ const Copyright: React.FC = () => {
     <Grid item xs={12} container justify="center">
       <Box paddingY="2rem">
         <Grid item container justify="center">
-          <CustomText
-            color="white"
-            fontSize="0.75rem"
-            text={i18n.t('footer:copyright')}
-          />
+          <Grid>
+            <CustomText
+              color="white"
+              fontSize="0.75rem"
+              text={i18n.t('footer:copyright-title')}
+            />
+          </Grid>
+          <CopyrightInfoGrid>
+            <CustomText
+              color="white"
+              fontSize="0.75rem"
+              text={i18n.t('footer:copyright-info')}
+            />
+          </CopyrightInfoGrid>
         </Grid>
         <Grid item container justify="center">
           <CustomText
+            alignment="center"
             color="white"
             fontSize="0.75rem"
             text={i18n.t('footer:author')}
